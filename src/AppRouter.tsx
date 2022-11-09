c37f0f488d472e6e6dd829153c7cb5f49a0b851e
joe-yarbrough <joe@afternightfall.com>
2022-11-08 21:37:22 -0600

    router update

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -4,24 +4,28 @@ import {
   ThemeProvider as MuiThemeProvider,
 } from '@mui/material/styles';
 import React, { useContext, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Route } from 'react-router';
 import { Store } from 'redux';
+import { Dispatch } from 'redux';
 
 import App from './App';
 import SignInScreen from './components/top-level-components/SignInScreen';
 import { appRoutes } from './configs/app-settings/app-routes';
 import { getTheme } from './configs/app-settings/theme';
+import { loggedInUser } from './creators/user';
 import { Initializer } from './firebase/Initializer';
 import { AuthContext } from './firebase/auth/AuthContext';
 
-export default function AppRouter(props: AppRouterProps): JSX.Element {
-  const { store } = props;
+const AppRouter = (props: AppRouterProps): JSX.Element => {
+  const { store, updateLoggedInUserHandler } = props;
   const user = useContext(AuthContext);
 
   useEffect(() => {
     if (user) {
       const initializer = new Initializer(store);
       initializer.initializeFirebase();
+      user.email && updateLoggedInUserHandler(user.email);
     }
   }, [user]);
 
@@ -57,8 +61,22 @@ export default function AppRouter(props: AppRouterProps): JSX.Element {
       </MuiThemeProvider>
     </StyledEngineProvider>
   );
-}
+};
 
-interface AppRouterProps {
+type AppRouterProps = PassedInProps & DispatchProps;
+
+interface PassedInProps {
   store: Store;
 }
+
+interface DispatchProps {
+  updateLoggedInUserHandler: (email: string) => void;
+}
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  updateLoggedInUserHandler: (email: string) => {
+    dispatch(loggedInUser(email));
+  },
+});
+
+export default connect(null, mapDispatchToProps)(AppRouter);
