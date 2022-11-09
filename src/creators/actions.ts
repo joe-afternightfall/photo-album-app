8d3e69202fd6b306f9f704011a067743865c406e
joe-yarbrough <joe@afternightfall.com>
2022-11-08 18:51:09 -0600

    actions

--- src/creators/actions.ts
+++ src/creators/actions.ts
@@ -1,12 +1,18 @@
 import { LocationChangeAction } from 'connected-react-router';
 
-import { LoadAlbumsAction } from './albums';
+import { LoadAlbumsAction, SaveNewAlbumAction } from './albums';
+import { LoggedInUserAction } from './user';
 
 export enum ActionTypes {
   LOGGED_IN_USER = 'LOGGED_IN_USER',
   TOGGLE_APP_SIDE_DRAWER = 'TOGGLE_APP_SIDE_DRAWER',
   UPDATE_APP_THEME = 'UPDATE_APP_THEME',
   LOAD_ALBUMS = 'LOAD_ALBUMS',
+  SAVE_NEW_ALBUM = 'SAVE_NEW_ALBUM',
 }
 
-export type ApplicationActions = LocationChangeAction | LoadAlbumsAction;
+export type ApplicationActions =
+  | LocationChangeAction
+  | LoadAlbumsAction
+  | LoggedInUserAction
+  | SaveNewAlbumAction;
