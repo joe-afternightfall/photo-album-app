6eee6eeab3f16ae5c3796b131dc7c8a92e2019fe
joe-yarbrough <joe@afternightfall.com>
2022-11-08 21:38:36 -0600

    new album service

--- src/services/firebase-albums-service.ts
+++ src/services/firebase-albums-service.ts
@@ -1,8 +1,18 @@
 import firebase from 'firebase/compat/app';
-
+import { Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import { v4 as uuidv4 } from 'uuid';
 import 'firebase/compat/database';
+
 import { FIREBASE_ALBUMS_ROUTE } from '../configs/firebase/firebase-routes';
 import { AlbumDAO, AlbumVO } from '../configs/interfaces';
+import { State } from '../configs/redux/store';
+import { ApplicationActions } from '../creators/actions';
+import { displayAppLoader, hideAppLoader } from '../creators/app-loader';
+import {
+  displayErrorSnackbar,
+  displaySuccessSnackbar,
+} from '../creators/app-snackbar';
 
 export const getAllAlbums = async (): Promise<AlbumVO[]> => {
   return await firebase
@@ -17,6 +27,7 @@ export const getAllAlbums = async (): Promise<AlbumVO[]> => {
             firebaseId: key,
             id: snap[key].id,
             title: snap[key].title,
+            subtitle: snap[key].subtitle,
             imageId: snap[key].imageId,
             created: snap[key].created,
             updated: snap[key].updated,
@@ -27,3 +38,46 @@ export const getAllAlbums = async (): Promise<AlbumVO[]> => {
       }
     });
 };
+
+export interface NewAlbumInfo {
+  title: string;
+  subtitle: string;
+  image?: File;
+}
+
+export const saveNewAlbum =
+  (
+    info: NewAlbumInfo,
+    successCallback?: () => void
+  ): ThunkAction<void, State, void, ApplicationActions> =>
+  async (dispatch: Dispatch): Promise<void> => {
+    dispatch(displayAppLoader());
+
+    const ref = firebase.database().ref(FIREBASE_ALBUMS_ROUTE);
+    const newRef = ref.push();
+
+    const timestamp = new Date().toLocaleString(); // ex from firebase: 11/8/2022, 9:15:09 PM
+
+    const newAlbum: AlbumDAO = {
+      id: uuidv4(),
+      title: info.title,
+      subtitle: info.subtitle,
+      imageId: '',
+      created: timestamp,
+      updated: timestamp,
+    };
+
+    return await newRef.set(newAlbum, (error: Error | null) => {
+      if (error) {
+        dispatch(displayErrorSnackbar(`Error creating album ${info.title}.`));
+      } else {
+        dispatch(
+          displaySuccessSnackbar(`Successfully created album ${info.title}.`)
+        );
+        successCallback && successCallback();
+        setTimeout(() => {
+          dispatch(hideAppLoader());
+        }, 1500);
+      }
+    });
+  };
