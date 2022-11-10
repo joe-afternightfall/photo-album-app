a4c4eba203219e37ba83632ed26746a5685f7b1c
joe-yarbrough <joe@afternightfall.com>
2022-11-10 08:38:43 -0600

    view update

--- src/components/widgets/list-view/ListView.tsx
+++ src/components/widgets/list-view/ListView.tsx
@@ -1,29 +1,67 @@
 import { useMediaQuery } from '@mui/material';
 import Box from '@mui/material/Box';
+import Fade from '@mui/material/Fade';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { useTheme } from '@mui/material/styles';
-import React from 'react';
+import { createStyles, makeStyles } from '@mui/styles';
+import React, { useState } from 'react';
 
 import { ImageVO } from '../../../configs/interfaces';
+import ImageToolbar from './components/ImageToolbar';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    imageListItem: {
+      border: '1px solid black',
+      '&:hover': {
+        cursor: 'pointer',
+      },
+    },
+  })
+);
 
 export default function ListView(props: ListViewProps): JSX.Element {
+  const classes = useStyles();
   const theme = useTheme();
   const { images } = props;
   const isXs = useMediaQuery(theme.breakpoints.down(700));
+  const isMd = useMediaQuery(theme.breakpoints.up('md'));
+  const [displayToolBar, setDisplayToolbar] = useState('');
 
   return (
     <Box>
       <ImageList variant="masonry" cols={isXs ? 2 : 4} gap={8}>
         {images.map((image) => {
           return (
-            <ImageListItem key={image.id}>
+            <ImageListItem
+              onMouseOver={() => {
+                setDisplayToolbar(image.id);
+              }}
+              onMouseLeave={() => {
+                setDisplayToolbar('');
+              }}
+              key={image.id}
+              className={classes.imageListItem}
+            >
               <img
                 src={image.downloadURL}
-                // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
                 alt={image.fileName}
                 loading="lazy"
+                style={{
+                  objectFit: 'contain',
+                }}
               />
+
+              {isMd ? (
+                <Fade in={displayToolBar === image.id}>
+                  <div>
+                    <ImageToolbar imageId={image.id} />
+                  </div>
+                </Fade>
+              ) : (
+                <ImageToolbar imageId={image.id} />
+              )}
             </ImageListItem>
           );
         })}
