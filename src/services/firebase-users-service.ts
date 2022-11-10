00b9f3fb38306bd503bff909da2da3d107c96ae3
joe-yarbrough <joe@afternightfall.com>
2022-11-10 08:36:12 -0600

    user service

--- src/services/firebase-users-service.ts
+++ src/services/firebase-users-service.ts
@@ -1,22 +1,26 @@
 import firebase from 'firebase/compat/app';
+import * as ramda from 'ramda';
+import { Dispatch } from 'redux';
+import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { v4 as uuidv4 } from 'uuid';
 
 import { auth } from '../configs/firebase/firebase-config';
 import { FIREBASE_USERS_ROUTE } from '../configs/firebase/firebase-routes';
 import { UserDAO } from '../configs/interfaces/user/UserDAO';
 import { UserVO } from '../configs/interfaces/user/UserVO';
+import { State } from '../configs/redux/store';
+import { ApplicationActions } from '../creators/actions';
+import { loggedInUser } from '../creators/user';
 import { generateTimestamp } from '../utils/timestamp-generator';
 
 export const getSignedInUserProfile = async (): Promise<UserVO | undefined> => {
   if (auth.currentUser) {
-    console.log('auth.currentUser: ' + JSON.stringify(auth.currentUser));
     return await firebase
       .database()
       .ref(FIREBASE_USERS_ROUTE + '/' + auth.currentUser.uid)
       .once('value')
       .then(async (snapshot) => {
         const snap = snapshot.val();
-        console.log('snap: ' + JSON.stringify(snap));
         if (snap) {
           const userVOS = Object.keys(snap).map((key: string): UserVO => {
             return {
@@ -24,7 +28,7 @@ export const getSignedInUserProfile = async (): Promise<UserVO | undefined> => {
               id: snap[key].id,
               username: snap[key].username,
               email: snap[key].email,
-              favoriteImageIds: snap[key].favoriteImageIds,
+              favoriteImageIds: snap[key].favoriteImageIds ?? [],
               created: snap[key].created,
               updated: snap[key].updated,
             };
@@ -64,3 +68,63 @@ export const saveNewUser = async (): Promise<UserVO> => {
     }
   });
 };
+
+export const removeImageFromUsersFavoriteList =
+  (imageId: string): ThunkAction<void, State, void, ApplicationActions> =>
+  async (dispatch: Dispatch, getState: () => State): Promise<void> => {
+    const signedInUser = getState().applicationState.signedInUser;
+    if (signedInUser) {
+      const clonedImageIds = ramda.clone(signedInUser.favoriteImageIds);
+      const foundImage = clonedImageIds.find((id) => id === imageId);
+
+      if (foundImage) {
+        const foundIndex = clonedImageIds.indexOf(foundImage);
+        if (foundIndex !== -1) {
+          clonedImageIds.splice(foundIndex, 1);
+
+          (dispatch as ThunkDispatch<State, void, ApplicationActions>)(
+            updateFavoritesList(clonedImageIds)
+          );
+        }
+      }
+    }
+  };
+
+export const tagImageAsFavorite =
+  (imageId: string): ThunkAction<void, State, void, ApplicationActions> =>
+  async (dispatch: Dispatch, getState: () => State): Promise<void> => {
+    const signedInUser = getState().applicationState.signedInUser;
+    if (signedInUser) {
+      const clonedImageIds = ramda.clone(signedInUser.favoriteImageIds);
+      clonedImageIds.push(imageId);
+
+      (dispatch as ThunkDispatch<State, void, ApplicationActions>)(
+        updateFavoritesList(clonedImageIds)
+      );
+    }
+  };
+
+const updateFavoritesList =
+  (imageIds: string[]): ThunkAction<void, State, void, ApplicationActions> =>
+  async (dispatch: Dispatch, getState: () => State): Promise<void> => {
+    const signedInUser = getState().applicationState.signedInUser;
+    if (signedInUser && auth.currentUser) {
+      return await firebase
+        .database()
+        .ref(FIREBASE_USERS_ROUTE + '/' + auth.currentUser.uid)
+        .child(signedInUser.firebaseId)
+        .update(
+          {
+            favoriteImageIds: imageIds,
+          },
+          async (error: Error | null) => {
+            if (error) {
+              console.error('error: ' + JSON.stringify(error));
+            } else {
+              const userProfile = await getSignedInUserProfile();
+              dispatch(loggedInUser(userProfile));
+            }
+          }
+        );
+    }
+  };
