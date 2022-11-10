75182f3c511988933100be2e0cb39cc66a6f03bc
joe-yarbrough <joe@afternightfall.com>
2022-11-10 08:36:41 -0600

    storage

--- src/services/firebase-upload-service.ts
+++ src/services/firebase-upload-service.ts
@@ -1,17 +1,13 @@
-// import firebase from 'firebase/app';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/storage';
-import { Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { v4 as uuidv4 } from 'uuid';
 
 import { FIREBASE_IMAGES_ROUTE } from '../configs/firebase/firebase-routes';
-import { ImageDAO, ImageVO } from '../configs/interfaces';
+import { ImageDAO } from '../configs/interfaces';
 import { State } from '../configs/redux/store';
 import { ApplicationActions } from '../creators/actions';
 import { generateTimestamp } from '../utils/timestamp-generator';
-
-// import FirebaseStorageError = firebase.storage.FirebaseStorageError;
 
 export const uploadImageFiles =
   (
@@ -19,22 +15,16 @@ export const uploadImageFiles =
     files: File[],
     callback?: () => void
   ): ThunkAction<void, State, void, ApplicationActions> =>
-  async (dispatch: Dispatch, getState: () => State): Promise<void> => {
-    const folderName = albumId;
-    // const errors = [];
-
+  async (): Promise<void> => {
     files.map(async (file) => {
       const storageRef = firebase
         .storage()
         .ref(`images/${albumId}/${file.name}`);
 
-      // await storageRef.put(file).catch((e) => errors.push(e));
       await storageRef.put(file);
 
       const downloadURL = await storageRef.getDownloadURL();
       const timestamp = generateTimestamp();
-
-      console.log('******* downloadURL: ' + JSON.stringify(downloadURL));
 
       const imageInfo: ImageDAO = {
         id: uuidv4(),
@@ -53,7 +43,7 @@ export const uploadImageFiles =
 
       return await newRef.set(imageInfo, (e: Error | null) => {
         if (e) {
-          console.log('e: ' + JSON.stringify(e));
+          console.error('e: ' + JSON.stringify(e));
         } else {
           console.log('success');
         }
@@ -61,12 +51,4 @@ export const uploadImageFiles =
     });
 
     callback && callback();
-
-    // if (errors.length > 0) {
-    //   dispatch(displayErrorSnackbar(`Error saving images ${errors[0].name}.`));
-    // } else {
-    //   dispatch(displaySuccessSnackbar('Successfully saved images'));
-    // }
   };
-
-// https://firebasestorage.googleapis.com/v0/b/(project id).appspot.com/o/(storage path)?alt=media
