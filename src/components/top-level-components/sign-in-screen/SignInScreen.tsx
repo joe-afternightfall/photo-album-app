e23e80a3110e18c098da38fde7445ab219dcd070
joe-yarbrough <joe@afternightfall.com>
2022-11-18 10:58:22 -0600

    sign in cleanup

--- src/components/top-level-components/sign-in-screen/SignInScreen.tsx
+++ src/components/top-level-components/sign-in-screen/SignInScreen.tsx
@@ -1,23 +1,30 @@
 import { useMediaQuery } from '@mui/material';
-import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
 import { useTheme } from '@mui/material/styles';
-import React, { ChangeEvent, useState } from 'react';
+import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
 
-import { auth } from '../../../configs/firebase/firebase-config';
+import RequestAccessForm from './components/RequestAccessForm';
+import SignInForm from './components/SignInForm';
+import TabPanel from './components/TabPanel';
 
 export default function SignInScreen(): JSX.Element {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [helperText, setHelperText] = useState('');
-  const [passwordError, setPasswordError] = useState(false);
-  const [emailError, setEmailError] = useState(false);
   const theme = useTheme();
   const isMd = useMediaQuery(theme.breakpoints.up('md'));
+  const [currentTab, setCurrentTab] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setCurrentTab(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setCurrentTab(index);
+  };
 
   // const createAccount = async () => {
   //   try {
@@ -31,32 +38,6 @@ export default function SignInScreen(): JSX.Element {
   //     console.error(error);
   //   }
   // };
-
-  const signIn = async () => {
-    try {
-      if (email !== '' && password !== '') {
-        await auth.signInWithEmailAndPassword(email, password);
-      }
-    } catch (e) {
-      const error = e as unknown as { code: string };
-      console.error('firebase-error: ' + JSON.stringify(error));
-      if (error.code === 'auth/user-not-found') {
-        setEmailError(true);
-        setPasswordError(false);
-        setHelperText('email not found');
-      } else if (error.code === 'auth/wrong-password') {
-        setEmailError(false);
-        setPasswordError(true);
-        setHelperText('wrong password');
-      } else if (error.code === 'auth/invalid-email') {
-        setEmailError(true);
-        setPasswordError(false);
-        setHelperText('not a valid email');
-      }
-    }
-  };
-
-  const canAttemptSignIn = email !== '' && password !== '';
 
   return (
     <Grid container alignItems="center" justifyContent="center">
@@ -77,53 +58,33 @@ export default function SignInScreen(): JSX.Element {
             transform: 'translate3d(-50%, -50%, 0)',
           }}
         >
-          <Card sx={{ minWidth: isMd ? '400px' : '300px', py: 8 }}>
-            <CardContent sx={{ px: 0 }}>
-              <Grid
-                container
-                spacing={2}
-                direction="column"
-                alignItems="center"
+          <Card sx={{ minWidth: isMd ? '400px' : '300px', pb: 8 }} square>
+            <CardContent sx={{ px: 0, pt: 0 }}>
+              <AppBar position="static">
+                <Tabs
+                  value={currentTab}
+                  onChange={handleChange}
+                  indicatorColor="secondary"
+                  textColor="inherit"
+                  variant="fullWidth"
+                >
+                  <Tab label="Sign In" />
+                  <Tab label="Request Access" />
+                </Tabs>
+              </AppBar>
+              <SwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={currentTab}
+                onChangeIndex={handleChangeIndex}
               >
-                <Grid item>
-                  <TextField
-                    fullWidth
-                    variant={'outlined'}
-                    label={'email'}
-                    value={email}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                      setEmail(e.target.value);
-                    }}
-                    error={emailError}
-                    helperText={emailError ? helperText : ''}
-                  />
-                </Grid>
-                <Grid item>
-                  <TextField
-                    fullWidth
-                    variant={'outlined'}
-                    label={'password'}
-                    type={'password'}
-                    value={password}
-                    error={passwordError}
-                    helperText={passwordError ? helperText : ''}
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                  />
-                </Grid>
-              </Grid>
+                <TabPanel value={currentTab} index={0} dir={theme.direction}>
+                  <SignInForm />
+                </TabPanel>
+                <TabPanel value={currentTab} index={1} dir={theme.direction}>
+                  <RequestAccessForm />
+                </TabPanel>
+              </SwipeableViews>
             </CardContent>
-            <CardActions>
-              <Button
-                variant={'contained'}
-                sx={{ m: 'auto' }}
-                onClick={signIn}
-                disabled={!canAttemptSignIn}
-              >
-                {'Sign In'}
-              </Button>
-            </CardActions>
           </Card>
         </Grid>
       </Grid>
