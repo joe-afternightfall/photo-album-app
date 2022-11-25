1cc050f3e83edac933f9e28d1d830d9959faf0af
joe-yarbrough <joe@afternightfall.com>
2022-11-25 15:16:15 -0600

    toolbar

---
+++ src/components/shared/skeleton/skeleton-image-toolbar/SkeletonImageToolbar.tsx
@@ -0,0 +1,19 @@
+import Grid from '@mui/material/Grid';
+import Skeleton from '@mui/material/Skeleton';
+import React from 'react';
+
+export default function SkeletonImageToolbar(): JSX.Element {
+  return (
+    <Grid item xs={3} container spacing={1} justifyContent="flex-end">
+      <Grid item xs={4}>
+        <Skeleton variant="rectangular" width="100%" height={10} />
+      </Grid>
+      <Grid item xs={4}>
+        <Skeleton variant="rectangular" width="100%" height={10} />
+      </Grid>
+      <Grid item xs={4}>
+        <Skeleton variant="rectangular" width="100%" height={10} />
+      </Grid>
+    </Grid>
+  );
+}
