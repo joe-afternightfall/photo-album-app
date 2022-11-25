0975efac00d0a1ac4579302e2a198fd6669806ed
joe-yarbrough <joe@afternightfall.com>
2022-11-25 11:58:51 -0600

    zipping

--- src/components/top-level-components/selected-album-screen/SelectedAlbumScreen.tsx
+++ src/components/top-level-components/selected-album-screen/SelectedAlbumScreen.tsx
@@ -1,14 +1,9 @@
-import DownloadIcon from '@mui/icons-material/Download';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import isEmpty from 'is-empty';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { AlbumVO, ImageVO } from '../../../configs/interfaces';
 import { State } from '../../../configs/redux/store';
-import { zipImages } from '../../../utils/save-images';
 import ListView from '../../widgets/views/list-view/ListView';
 
 const SelectedAlbumScreen = (props: SelectedAlbumScreenProps): JSX.Element => {
@@ -23,46 +18,7 @@ const SelectedAlbumScreen = (props: SelectedAlbumScreenProps): JSX.Element => {
 
   const imagesToDisplay = displayFavorites ? favoriteImages : albumImages;
 
-  return (
-    <Grid container>
-      <Grid
-        item
-        xs={12}
-        container
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Grid item>
-          <Typography variant="h6" color="textSecondary">
-            {selectedAlbum && selectedAlbum.title}
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Grid container alignItems="center">
-            <Grid item>
-              <Button
-                variant="outlined"
-                startIcon={<DownloadIcon />}
-                onClick={() => {
-                  selectedAlbum &&
-                    zipImages(
-                      `${selectedAlbum.title}-favorites`,
-                      favoriteImages
-                    );
-                }}
-              >
-                {'Download Favs'}
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
-
-      <Grid item xs={12}>
-        <ListView images={imagesToDisplay} />
-      </Grid>
-    </Grid>
-  );
+  return <ListView images={imagesToDisplay} />;
 };
 
 type SelectedAlbumScreenProps = StateProps;
---
+++ src/utils/zip-images.ts
@@ -0,0 +1,43 @@
+import { Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+
+import { ImageVO } from '../configs/interfaces';
+import { State } from '../configs/redux/store';
+import { ApplicationActions } from '../creators/actions';
+import {
+  displayErrorSnackbar,
+  displaySuccessSnackbar,
+} from '../creators/app-snackbar';
+import { zipImages } from './save-images';
+
+export const zipAndSaveSelectedAlbumFavorites =
+  (): ThunkAction<void, State, void, ApplicationActions> =>
+  async (dispatch: Dispatch, getState: () => State): Promise<void> => {
+    const state = getState();
+
+    const signedInUser = state.applicationState.signedInUser;
+    const selectedAlbum = state.selectedAlbumState.currentAlbum;
+    const albumImages = state.selectedAlbumState.albumImages;
+    const favoriteImages: ImageVO[] = [];
+
+    if (signedInUser && signedInUser.favoriteImageIds.length) {
+      signedInUser.favoriteImageIds.map((favId) => {
+        albumImages.find((image) => {
+          if (image.id === favId) {
+            favoriteImages.push(image);
+          }
+        });
+      });
+    }
+
+    if (selectedAlbum && favoriteImages.length > 0) {
+      zipImages(`${selectedAlbum.title}-favorites`, favoriteImages)
+        .catch((e) => {
+          console.log('e: ' + JSON.stringify(e));
+          dispatch(displayErrorSnackbar(`Error downloading favorite's`));
+        })
+        .then(() => {
+          dispatch(displaySuccessSnackbar(`Downloaded selected favorite's`));
+        });
+    }
+  };
