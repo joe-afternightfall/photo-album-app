bdd2c1758f33908f7e4c294a1278128236ad0801
joe-yarbrough <joe@afternightfall.com>
2022-11-25 22:31:02 -0600

    profile setup

---
+++ src/components/app-shell/app-bar/components/user-profile/UserProfile.tsx
@@ -0,0 +1,57 @@
+import Logout from '@mui/icons-material/Logout';
+import Avatar from '@mui/material/Avatar';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Tooltip from '@mui/material/Tooltip';
+import * as React from 'react';
+
+import { auth } from '../../../../../configs/firebase/firebase-config';
+import ProfileHeader from './ProfileHeader';
+
+export default function UserProfile() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const signOut = async () => {
+    window.location.replace('/');
+    await auth.signOut();
+  };
+
+  return (
+    <>
+      <Tooltip title="User Profile">
+        <IconButton onClick={handleClick} size="small">
+          <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        onClick={handleClose}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        <ProfileHeader />
+        <Divider />
+        <MenuItem onClick={signOut}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </>
+  );
+}
