0e3ef5c21570f1899ae8dbe60edfa0af84a38253
joe-yarbrough <joe@afternightfall.com>
2022-11-25 21:55:23 -0600

    new toolbar

--- src/components/app-shell/app-bar/variants/SelectedAlbumToolbar.tsx
+++ src/components/app-shell/app-bar/variants/SelectedAlbumToolbar.tsx
@@ -1,9 +1,95 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { routerActions } from 'connected-react-router';
 import React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 
-export default function SelectedAlbumToolbar(): JSX.Element {
+import { AppPaths } from '../../../../configs/app-settings/app-routes';
+import { State } from '../../../../configs/redux/store';
+import ActionMenu from '../ActionMenu';
+import ImageAccessTypeSelectMenu from '../components/ImageAccessTypeSelectMenu';
+
+const SelectedAlbumToolbar = (
+  props: SelectedAlbumToolbarProps
+): JSX.Element => {
+  const { title, goBackHandler, userIsAdmin } = props;
+
   return (
-    <div>
-      <p>Functional Connected Component</p>
-    </div>
+    <Toolbar>
+      <IconButton
+        color="inherit"
+        edge="start"
+        sx={{
+          mr: 2,
+        }}
+        onClick={goBackHandler}
+      >
+        <ArrowBackIcon />
+      </IconButton>
+      <Grid
+        item
+        xs={12}
+        container
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Grid item>
+          <Typography variant="h6" noWrap component="div">
+            {title}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Grid container alignItems="center">
+            {userIsAdmin && (
+              <Grid item>
+                <ImageAccessTypeSelectMenu />
+              </Grid>
+            )}
+            <Grid item>
+              <ActionMenu />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Toolbar>
   );
+};
+
+type SelectedAlbumToolbarProps = StateProps & DispatchProps;
+
+interface StateProps {
+  userIsAdmin: boolean;
+  title: string;
 }
+
+interface DispatchProps {
+  goBackHandler: () => void;
+}
+
+const mapStateToProps = (state: State): StateProps => {
+  const currentAlbum = state.selectedAlbumState.currentAlbum;
+  let title = '';
+  if (currentAlbum) {
+    title = currentAlbum.title;
+  }
+
+  return {
+    title,
+    userIsAdmin: Boolean(state.applicationState.signedInUser?.isAdmin),
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  goBackHandler: () => {
+    dispatch(routerActions.push(AppPaths.dashboard));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SelectedAlbumToolbar);
