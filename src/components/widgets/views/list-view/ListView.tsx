d7f7c7ea5747dcf5dbb091f6e867c2e21ef9926f
joe-yarbrough <joe@afternightfall.com>
2022-11-27 23:22:52 -0600

    infinite scrolling

--- src/components/widgets/views/list-view/ListView.tsx
+++ src/components/widgets/views/list-view/ListView.tsx
@@ -1,10 +1,21 @@
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import * as ramda from 'ramda';
 import React, { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 import { ImageVO } from '../../../../configs/interfaces';
 import ListViewLightbox from './lightbox/ListViewLightbox';
 import ListViewImageItem from './masonry-image/ListViewImageItem';
+
+const getImages = (images: ImageVO[]): ImageVO[] => {
+  if (images.length >= 10) {
+    return images.slice(0, 10);
+  }
+  return images;
+};
 
 export default function ListView(props: Props): JSX.Element {
   const { images } = props;
@@ -14,29 +25,63 @@ export default function ListView(props: Props): JSX.Element {
     index: -1,
   };
 
+  const [hasMore, setHasMore] = useState(true);
   const [displayLightbox, setDisplayLightbox] = useState(defaultDisplay);
+  const [localImages, setLocalImages] = useState<ImageVO[]>(getImages(images));
+
+  const fetchMoreData = () => {
+    const clonedImages = ramda.clone(images);
+    const clonedLocalImages = ramda.clone(localImages);
+    const startIndex = localImages.length;
+
+    for (let i = startIndex; i < startIndex + 5; ++i) {
+      const imageVO = clonedImages[i];
+      imageVO && clonedLocalImages.push(imageVO);
+    }
+
+    setHasMore(!(clonedLocalImages.length === images.length));
+    setLocalImages(clonedLocalImages);
+  };
 
   return (
     <Box sx={{ pt: 3 }}>
-      <ResponsiveMasonry>
-        <Masonry gutter="12px">
-          {images.map((image, index) => {
-            return (
-              <ListViewImageItem
-                key={image.id}
-                image={image}
-                openLightboxHandler={() => {
-                  setDisplayLightbox({
-                    open: true,
-                    downloadURL: image.downloadURL,
-                    index: index,
-                  });
-                }}
-              />
-            );
-          })}
-        </Masonry>
-      </ResponsiveMasonry>
+      <InfiniteScroll
+        dataLength={localImages.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Typography>{'Loading...'}</Typography>
+            </Grid>
+          </Grid>
+        }
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <ResponsiveMasonry>
+          <Masonry gutter="12px">
+            {localImages.map((image, index) => {
+              return (
+                <ListViewImageItem
+                  key={image.id}
+                  image={image}
+                  openLightboxHandler={() => {
+                    setDisplayLightbox({
+                      open: true,
+                      downloadURL: image.downloadURL,
+                      index: index,
+                    });
+                  }}
+                />
+              );
+            })}
+          </Masonry>
+        </ResponsiveMasonry>
+      </InfiniteScroll>
 
       <ListViewLightbox
         open={displayLightbox.open}
