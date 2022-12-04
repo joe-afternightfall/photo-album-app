2172ebce9bc777944500cdbd9eda33cbcedfb698
joe-yarbrough <joe@afternightfall.com>
2022-12-04 13:55:10 -0600

    new var

--- src/configs/firebase/firebase-routes.ts
+++ src/configs/firebase/firebase-routes.ts
@@ -2,3 +2,5 @@ export const FIREBASE_ALBUMS_ROUTE = '/albums';
 export const FIREBASE_IMAGES_ROUTE = '/images';
 export const FIREBASE_USERS_ROUTE = '/users';
 export const FIREBASE_REQUEST_ACCESS_ROUTE = '/new-user-request';
+export const FIREBASE_STORAGE_COMPRESSED_IMAGE_ROUTE = 'compressed-images';
+export const FIREBASE_STORAGE_ORIGINAL_IMAGE_ROUTE = 'original-images';
