07749cd02c47af4bf1f7e77fccf3cecdfbd4b006
joe-yarbrough <joe@afternightfall.com>
2022-12-04 23:25:30 -0600

    adding prop

--- src/components/shared/dropzone/DropZone.tsx
+++ src/components/shared/dropzone/DropZone.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { DropzoneArea } from 'react-mui-dropzone';
 
 export default function PaperDropzone(props: PaperDropzoneProps): JSX.Element {
-  const { dropzoneHandler, filesLimit } = props;
+  const { dropzoneHandler, onDropHandler, filesLimit } = props;
   return (
     <DropzoneArea
       onChange={dropzoneHandler}
       showFileNames
-      dropzoneText={'Drag and Drop file'}
+      dropzoneText="Drag and Drop file"
       showAlerts={false}
       filesLimit={filesLimit ? filesLimit : 50}
       showPreviewsInDropzone
       maxFileSize={1000000000}
+      onDrop={() => {
+        onDropHandler && onDropHandler();
+      }}
     />
   );
 }
@@ -19,4 +22,5 @@ export default function PaperDropzone(props: PaperDropzoneProps): JSX.Element {
 interface PaperDropzoneProps {
   filesLimit?: number;
   dropzoneHandler: (files: File[]) => void;
+  onDropHandler?: () => void;
 }
